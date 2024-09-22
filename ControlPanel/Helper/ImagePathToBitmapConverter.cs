2a71b73e414b9aee66c9a8574ec30837540f122c
Natfaji_A <modemoon2003@hotmail.com>
2024-09-22 20:19:27 +0200

    add helper method

---
+++ ControlPanel/Helper/ImagePathToBitmapConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+public class ImagePathToBitmapConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (value is string imagePath)
+		{
+			return new BitmapImage(new Uri($"pack://application:,,,/images/{imagePath}"));
+		}
+		return null;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
